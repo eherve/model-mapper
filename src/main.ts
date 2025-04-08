c398e84fc18b458fcb5406ae544115e1bfa448c4
eherve <eric.herve.95@gmail.com>
2025-04-08 12:02:12 +0200

    fix export Discriminator

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 /** @format */
 
-export {IMappedEntity} from './mapped-entity.interface';
-export {ModelMapper} from './model-mapper';
-export {IPropertyMapOptions} from './property-map-options.interface';
-export {propertyMap, PropertyMap} from './property-map.decorator';
-export {IPropertyMappingTreeElmt, PropertyMappingTree} from './property-mapping-tree.interface';
+export { IMappedEntity } from './mapped-entity.interface';
+export { ModelMapper } from './model-mapper';
+export { IPropertyMapOptions } from './property-map-options.interface';
+export { propertyMap, PropertyMap } from './property-map.decorator';
+export { Discriminator } from './discriminator.decorator';
+export { IPropertyMappingTreeElmt, PropertyMappingTree } from './property-mapping-tree.interface';
